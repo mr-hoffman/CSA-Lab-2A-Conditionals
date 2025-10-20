745b9afb6d819496b0d050d7c9ac1c3d713309ef
mr-hoffman <hoffmant@hilbertk12.org>
2025-10-20 11:02:09 -0500

    Refactor Lottery class and main method structure

--- Lottery.java
+++ Lottery.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
 
-public static void main(string[] args) {
-  // call your methods here
+public class Lottery {
+  public static void main(string[] args) {
+    // call your methods here
+    
+  }
   
+  //  write any helper methods here
+
 }
 
-
-//  write any helper methods here
